3620d4bd614eed0c0d9a6be573ab7d819e6bbc37
Jason Kim <actcrazy@gmail.com>
2022-07-16 11:55:04 +0900

    macro 5.6 repl에 macro 적용

--- lib/repl.ml
+++ lib/repl.ml
@@ -21,7 +21,7 @@ let print_error errors =
   print_endline "Woops! We ran into some monkey business here!";
   print_endline errors
 
-let print env code =
+let print env macro_env code =
   let go (p : Parser.t) env stmt =
     if List.length p.errors > 0 then (
       p.errors |> parse_list "\t" |> print_error;
@@ -34,15 +34,19 @@ let print env code =
   let l = Lexer.make code in
   let p = Parser.make l in
   let p, stmt = Parser.parse_program p in
-  go p env stmt
+  let macro_env, define_macros = Macro_expansion.define_macros macro_env stmt in
+  let expanded = Macro_expansion.expand_macros macro_env define_macros in
+  (go p env expanded, macro_env)
 
 let run () =
-  let rec go env =
+  let rec go env macro_env =
     try
       print_string prompt;
       let line = read_line () in
-      let env = print env line in
-      go env
+      let env, macro_env = print env macro_env line in
+      go env macro_env
     with
     | End_of_file -> () in
-  go (Environment.make ())
+  let env = Environment.make () in
+  let macro_env = Environment.make () in
+  go env macro_env
